57a73e1ed79e16300ce5f71047718e73a6a1d3a1
jagbegbo@gmail.com <jagbegbo@gmail.com>
2022-11-21 11:37:23 +0000

    banner started

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      'bleu': '#64C5BB',
+      'vert': '#099408',
+      'blanc': '#FFFFFF',
+      'noire': '#000000',
+    },
     extend: {},
   },
   plugins: [],
